ae29cabd34706e8186f12a19d5835328362a215a
cmpsaints <cmpsaints@yandex.com>
2019-03-21 16:07:54 -0700

    additions/changes

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ var express = require("express");
 //var expresshandlebars = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
