3f1c55986026edae469c0570c85666ab523101cd
cmpsaints <cmpsaints@yandex.com>
2019-03-21 16:40:15 -0700

    additions/changes

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 // start server, listen on PORT
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + " ...");
