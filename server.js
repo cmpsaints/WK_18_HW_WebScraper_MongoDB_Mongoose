f446ceceb344fab2ae743d8af5f343d9879c3302
cmpsaints <cmpsaints@yandex.com>
2019-03-21 16:35:37 -0700

    additions/changes

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ var mongoose = require("mongoose");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+// require models
+var db = require("./models");
+
+const PORT = process.env.PORT || 3000;
+
+// initiate Express
+var app = express();
+
+// middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// start server, listen on PORT
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + " ...");
+});
