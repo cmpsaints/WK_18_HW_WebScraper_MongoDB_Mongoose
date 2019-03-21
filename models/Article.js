21313630d5396a72956143275133c6690230c0fb
cmpsaints <cmpsaints@yandex.com>
2019-03-21 16:12:26 -0700

    additions/changes

---
+++ models/Article.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+// Schema constructor to create a new user schema object
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// create model from above schema (Mongoose's model method)
+var Article = mongoose.model("Article", ArticleSchema);
+
+// export Article model
+module.exports = Article;
